13119b388d0c3bb01acd9030df349e613bda2953
Geekigen <kigencaleb50@gmail.com>
2024-03-13 12:57:06 +0300

    initial commit

---
+++ twoSum.py
@@ -0,0 +1,19 @@
+# Problem: Given nums, an array of integers, and target,
+# find indices of the two numbers whose sum equals target.
+def twoSum(num, target):
+    temp = {}
+    for index, value in enumerate(num):
+        if target - value not in temp:
+            temp[value] = index
+            continue
+        return [temp[target - value], index]
+    print(temp)
+print(twoSum([2, 7, 11, 15], 9))
+
+# Summary . here we will take the array and the target
+# if you know a little bit of math ,x+y = c ,to make x the subject, x = c-y
+# so in this case we use the same approach to solve this leet .
+# step 1 is to initialize an empty dictionary .
+# step 2 is to loop through the array while using enumerate
+# to get both the index and the value of the key in each array usinf the for loop
+# step 3 is to if target 

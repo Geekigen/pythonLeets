7250d9bfef6750169a426969908c57308d90d773
Geekigen <kigencaleb50@gmail.com>
2024-03-17 13:59:52 +0300

    two sum solution two

--- twoSum.py
+++ twoSum.py
@@ -8,6 +8,7 @@ def twoSum(num, target):
             continue
         return [temp[target - value], index]
     print(temp)
+
 print(twoSum([2, 7, 11, 15], 9))
 
 # Summary . here we will take the array and the target
@@ -16,4 +17,12 @@ print(twoSum([2, 7, 11, 15], 9))
 # step 1 is to initialize an empty dictionary .
 # step 2 is to loop through the array while using enumerate
 # to get both the index and the value of the key in each array usinf the for loop
-# step 3 is to if target 
+# step 3 is to if target minus value is in the dicionary then the loop stops giving us the two numbers necessary
+# to add to the given target
+# This also acts as the base case for the loop
+#  the second way of doing it
+def twoSum2(numberlist,target):
+    return[[x, y] for x in range(len(numberlist)) for y in range(len(numberlist)) if numberlist[x] + numberlist[y] == target]
+
+print(twoSum2([1,2,3,4,5,6,7],9))
+# the second option is more dynamic and more simplified
